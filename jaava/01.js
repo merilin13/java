20e13d2ffb6ccf5c5d18e1ede2da51db3d5b06ea
merilin13 <150146754+merilin13@users.noreply.github.com>
2024-04-23 10:37:27 +0300

    Add files via upload

---
+++ jaava/01.js
@@ -0,0 +1,26 @@
+//Merilin Hiis
+//22.04.2024
+//1
+const num = 4.2;
+console.log(typeof x); // Output: "number"
+
+const text = "Will code 4 food";
+console.log(typeof y); // Output: "string"
+
+const number = 123;
+console.log(typeof l); // Output: "number"
+
+const decision = true;
+console.log(typeof z); // Output: "boolean"
+
+const array = [1,2,3];
+console.log(typeof a) //output: "object"
+
+const decisionprt2 = FALSE;
+console.log(typeof m); // Output: "boolean"
+
+const obj = 'true';
+console.log(typeof obj); // Output: "object"
+
+const mis = undefined;
+console.log(typeof w); // Output: "undefined"
